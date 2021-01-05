3162cdf5af2a3aec8d4ef2c6e6386b7095e01f30
y369q369 <1486866853@qq.com>
2021-01-05 17:28:46 +0800

    初始化

---
+++ src/main/java/com/example/bigdatastudy/BigdataStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.bigdatastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigdataStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigdataStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bigdatastudy/BigdataStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bigdatastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigdataStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
