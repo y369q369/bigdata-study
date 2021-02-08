f035bb8771bcd666e3a87e9a4cf7d4613b2e375c
y369q369 <1486866853@qq.com>
2021-02-08 14:35:48 +0800

    hbase的连接

--- springboot-api/src/main/java/com/example/springbootApi/service/HbaseService.java
+++ springboot-api/src/main/java/com/example/springbootApi/service/HbaseService.java
@@ -1,8 +1,15 @@
 package com.example.springbootApi.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.TableDescriptor;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * @Author grassPrince
@@ -11,11 +18,38 @@ import org.springframework.stereotype.Service;
  *  *              参考文档：
  **/
 @Service
+@Slf4j
 public class HbaseService {
 
-    public void getHBaseAdmin() {
+    /**
+     * 创建Hbase连接
+     * @return
+     * @throws IOException
+     */
+    public Connection getHBaseAdmin(){
         Configuration configuration = HBaseConfiguration.create();
-        configuration.set("hbase.zookeeper.quorum", "");
+        configuration.set("hbase.zookeeper.quorum", "120.26.184.85");
+        configuration.set("hbase.zookeeper.property.clientPort", "2181");
+        Connection connection = null;
+        try {
+            connection = ConnectionFactory.createConnection(configuration);
+        } catch (IOException e) {
+            log.error("创建HBase连接失败", e);
+        }
+        return connection;
     }
 
+    public void getTableList() throws IOException {
+        Connection connection = getHBaseAdmin();
+        List<TableDescriptor> list = connection.getAdmin().listTableDescriptors();
+        for (TableDescriptor tableDescriptor: list) {
+            log.info("table: {}, columns: {}", tableDescriptor.getTableName(), tableDescriptor.getColumnFamilies().toString());
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        HbaseService hbaseService = new HbaseService();
+        hbaseService.getTableList();
+    }
 }
