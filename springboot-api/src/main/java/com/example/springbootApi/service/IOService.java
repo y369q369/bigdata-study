e44d4a84ff1a7c1a6e34293398d7f3e76fbd6f1d
y369q369 <1486866853@qq.com>
2021-02-24 17:37:03 +0800

    下载

--- springboot-api/src/main/java/com/example/springbootApi/service/IOService.java
+++ springboot-api/src/main/java/com/example/springbootApi/service/IOService.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
+import java.util.Random;
 
 /**
  * @Author grassPrince
@@ -16,7 +17,7 @@ import java.io.*;
 public class IOService {
 
     /**
-     * 根据文件路径读取文件
+     * 字符流 根据文件路径读取文件
      * @param filePath 文件路径
      * @throws IOException
      */
@@ -30,6 +31,11 @@ public class IOService {
         }
     }
 
+    /**
+     * 字节流 根据文件路径读取文件
+     * @param filePath 文件路径
+     * @throws IOException
+     */
     public void readFile2(String filePath) throws IOException {
         File file = new File(filePath);
         int size = 200;
@@ -52,9 +58,48 @@ public class IOService {
         }
     }
 
+    /**
+     * 字符流 写入
+     * @param filePath 文件路径
+     * @throws IOException
+     */
+    public void write(String filePath) throws IOException {
+        // 直接覆盖文件内容
+//        FileWriter fw = new FileWriter(filePath);
+        // 文件中追加
+        FileWriter fw = new FileWriter(filePath, true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        for (int i = 0; i < 10; i++) {
+            bw.write(String.valueOf(new Random().nextInt(1000000)));
+            bw.newLine();
+        }
+
+        if (bw != null) {
+            bw.close();
+        }
+        if (fw != null) {
+            fw.close();
+        }
+        log.info("字符流写入完成");
+    }
+
+    public void write2(String filePath) throws IOException {
+        // 覆盖原有内容
+//        FileOutputStream fs = new FileOutputStream(filePath);
+        // 追加内容
+        FileOutputStream fs = new FileOutputStream(filePath, true);
+        for (int i = 0; i < 10; i++) {
+            fs.write(String.valueOf(new Random().nextInt(1000000)).getBytes());
+            fs.write("\n".getBytes());
+        }
+        log.info("字节流写入完成");
+    }
+
     public static void main(String[] args) throws IOException {
         IOService ioService = new IOService();
 //        ioService.readFile("C:\\Users\\grassprince\\Desktop\\src2\\main\\java\\io\\transwarp\\config\\Test.java");
-        ioService.readFile2("C:\\Users\\grassprince\\Desktop\\src2\\main\\java\\io\\transwarp\\config\\Test.java");
+//        ioService.readFile2("C:\\Users\\grassprince\\Desktop\\src2\\main\\java\\io\\transwarp\\config\\Test.java");
+//        ioService.write("./test.txt");
+        ioService.write2("./test2.txt");
     }
 }
